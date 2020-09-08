0461c77b200f772e72467ef1bf36da6ea30d9760
Aria Jones <13852374+ariajones@users.noreply.github.com>
2020-09-09 08:57:09 +1200

    Add data file

---
+++ data/data.js
@@ -0,0 +1,28 @@
+const whakataukī = {
+  kei_oou_ringaringa: [
+    {
+      id: 1,
+      whakataukī: 'Kei ōu ringaringa te ao',
+      image: '/1.jpg',
+      reo_question: 'He aha te kaupapa o teenei whakataukī?',
+      eng_question: 'What is this whakataukī all about?',
+      reo_description: 'He aha tō hiahia? Nōu te ao, hoea tō waka!... haere tonu enei description',
+      eng_description: 'What is it that you want? The world is yours, so go ahead! Make the most of opportunities given to you.',
+      kupu: ['Kei', 'ōu', 'ringaringa', 'te', 'ao']
+    }
+  ],
+  he_iti_hau_marangai: [
+    {
+      id: 2,
+      whakataukī: 'He iti hau marangai, e tū te pāhokahoka',
+      image: '/2.jpg',
+      reo_question: 'He aha te kaupapa o teenei whakataukī?',
+      eng_question: 'What is this whakataukī all about?',
+      reo_description: '... haere tonu ēnei description',
+      eng_description: 'This too shall pass...? the rainbow comes after the storm, success follows failure.',
+      kupu: ['He', 'iti', 'hau', 'marangai', 'e', 'tu', 'te', 'pahokahoka']
+    }
+  ]
+}
+
+export default whakataukī
