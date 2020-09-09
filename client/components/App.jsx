cacb35d8897e0207025b12fe5789aaf2213a1c9d
Aria Jones <13852374+ariajones@users.noreply.github.com>
2020-09-09 18:17:18 +1200

    Rewrite to use routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Home from './Home'
 
 const App = () => {
   return (
-    <>
-    <h1>Learn Te Reo with Whakataukī</h1>
-    <h2>E Hoa, kōwhiti tō Whakataukī!</h2>
-</>
+    <div>
+    <h1>Learn Te Reo With Whakataukī</h1>
+    <Route path='/' component={Home} />
+    <h2>E Hoa mā, kōwhiti tō whakataukī!</h2>
+</div>
   )
 }
 
