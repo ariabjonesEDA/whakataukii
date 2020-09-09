3e0b69b6559808558fb0214d901af6684ba813d2
Aria Jones <13852374+ariajones@users.noreply.github.com>
2020-09-09 18:17:53 +1200

    Comment out unnecessary knex db stuff for now

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,10 @@
 
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
-const db = require('knex')(config)
+// const environment = process.env.NODE_ENV || 'development'
+// const config = require('./knexfile')[environment]
+// const db = require('knex')(config)
 
-module.exports = {
-//  call your functions here
-}
+// module.exports = {
+// //  call your functions here
+// }
 
-// Add functions here
+// // Add functions here
