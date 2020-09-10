d7550cbd150cc487a6e5eef2e6b699d3a194ba58
Aria Jones <13852374+ariajones@users.noreply.github.com>
2020-09-10 13:58:02 +1200

    Get image path correct to display whakataukii cube

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-
+// import Whakataukii from './images/kei_oou_ringaringa'
 
 class Home extends React.Component {
   render () {
@@ -8,7 +8,13 @@ class Home extends React.Component {
       <div className="home">
         <h2>E Hoa mā, kōwhiti tō whakataukī!</h2>
         <p className='instruction'>Hello friends! listen to each proverb and click on a proverb to start learning!</p>
-        {/* <img className="img" src="images/whakarongo.png" alt="whakarongo listen"/> */}
+        <div className='display-cube-tahi'>
+        <img className="img-wh" src='./images/kei_oou_ringaringa.png' alt="te_ao_in_your_hands"/>
+        </div> 
+{/* 
+        <div className='display-cube-rua'>
+        <img className="img-wh" src='./images/paahokahoka.png' alt="te_ao_in_your_hands"/>
+        </div>  */}
         
       </div>
     )
